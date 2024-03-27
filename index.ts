747202373fe78eda89638e6b2d4c75e13d160ead
usaidkhan123 <161123814+usaidkhan123@users.noreply.github.com>
2024-03-28 01:26:36 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+
+
+
+const randomnumber =Math.floor(Math.random()*6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "Guess a number between 1 to 6",
+    }
+]);
+if(answer.userguessednumber === randomnumber){
+    console.log("You got it right!");
+}else {
+    console.log("You got it wrong!");
+}
